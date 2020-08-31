ef4d6e63cf47b7aef380d7868900696bc01729f4
UllSif <weckering.guillaume@live.fr>
2020-08-31 10:46:07 +0200

    twitter api connection

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+// const tweetsRoute = require('../tweets')
+
+const Twit = require('twit');
+var T = new Twit({
+    consumer_key: 'mPX22oQC6ACmDjUYAsqC3MOjL',
+    consumer_secret: 'XaVNyDqQU4ZBfINA3ykTAlEQKBh2IIQb1T5gHliodbngRhH5rn',
+    access_token: '1283450833330470914-vSKQOIA6eKZS4CNIBj2CdCvGLh6GPB',
+    access_token_secret: '9s6TdkYHzV9zjDrlZ4MbjEn3f8JyzwxIWTxGbM1G1A1Vv'
+})
+
+
+// DATABASE CONNECTION
+mongoose.connect('mongodb://localhost:27017/mytweets', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connecting error'));
+db.once('open', function () {
+    // we're connected!
+    console.log('DB connected on port 27017');
+});
+
+
+// CREATE APP CONF
+app.use('lib', express.static(__dirname + '/client/static'));
+
+// CREATE ROUTES API
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/static/index.html');
+});
+
+app.get('/tweets', function (req, res) {
+    res.sendFile(__dirname + '/client/static/tweets.html')
+})
+
+// START server
+app.listen(3000, function () {
+    console.log('App listening on port 3000');
+})
